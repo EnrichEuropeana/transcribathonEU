48a0ff725adf41a47f442a30fcb8c4ffd324c6f1
Marius Meller <marius_meller@hotmail.de>
2019-05-06 11:48:00 +0200

    Different js functions and css additions

--- wp-content/themes/transcribathon/js/custom.js
+++ wp-content/themes/transcribathon/js/custom.js
@@ -1,26 +1,69 @@
-function switchTab(event, tabName) {
-    
-    var i, tabcontent, tablinks;
+// Switches between different tabs within the item page image view
+function switchItemTab(event, tabName) {
+  var i, tabcontent, tablinks;
+
+  // Hide all tab contents
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+  // Make tab icon inactive
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabName).style.display = "block";
-  event.currentTarget.className += " active";
-  /*
-    //jQuery('#panel-right-content').html(tab);
-    jQuery('#editor-tab').css('display', 'none');
-    jQuery('#equilize-tab').css('display', 'none');
-    jQuery('#info-tab').css('display', 'none');
-    jQuery('#tags-tab').css('display', 'none');
-    jQuery('#query-tab').css('display', 'none');
 
-    jQuery('#' + tab + '-tab').css('display', 'block');
-    */
+  // Show clicked tab content and make icon active
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";    
+}
+
+// Switches between different views within the item page image view
+function switchItemView(viewName) {
+  switch(viewName) {
+    case 'horizontal':
+      jQuery("#item-image-section").css("height", '100%')
+      jQuery("#item-image-section").css("width", 'calc(100% - 450px)')
+      jQuery("#image-view-container").removeClass("panel-container-vertical")
+      jQuery("#image-view-container").addClass("panel-container-horizontal")
+      jQuery("#item-image-section").removeClass("panel-top")
+      jQuery("#item-image-section").addClass("panel-left")
+      jQuery("#item-data-section").removeClass("panel-bottom")
+      jQuery("#item-data-section").addClass("panel-right")
+      jQuery("#item-splitter").removeClass("splitter-horizontal")
+      jQuery("#item-splitter").addClass("splitter-vertical")
+      jQuery("#item-image-section").resizable({
+          handleSelector: "#item-splitter",
+          resizeHeight: false,
+          resizeWidth: true
+      });
+      jQuery("#item-image-section").css('min-width', '250px')
+      jQuery("#item-image-section").css('min-height', '100%')
+      break;
+    case 'vertical':
+      jQuery("#item-image-section").css("width", '100%')
+      jQuery("#item-image-section").css("height", 'calc(100% - 300px)')
+      jQuery("#image-view-container").removeClass("panel-container-horizontal")
+      jQuery("#image-view-container").addClass("panel-container-vertical")
+      jQuery("#item-image-section").removeClass("panel-left")
+      jQuery("#item-image-section").addClass("panel-top")
+      jQuery("#item-data-section").removeClass("panel-right")
+      jQuery("#item-data-section").addClass("panel-bottom")
+      jQuery("#item-splitter").removeClass("splitter-vertical")
+      jQuery("#item-splitter").addClass("splitter-horizontal")
+      jQuery("#item-image-section").resizable({
+          handleSelector: "#item-splitter",
+          resizeHeight: true,
+          resizeWidth: false
+      });
+      jQuery("#item-image-section").css('min-width', '100%')
+      jQuery("#item-image-section").css('min-height', '175px')
+      break;
+  }
+}
+
+
+// Calls script to draw linechart on the profile page
 function getTCTlinePersonalChart(what,start,ende,holder,uid){
 	"use strict";
   jQuery.post("/wp-content/themes/transcribathon/admin/inc/custom_profiletabs/scripts/linechart-script.php",
@@ -44,5 +87,82 @@ function getTCTlinePersonalChart(what,start,ende,holder,uid){
 	});
 }
 
+// Compares two transcriptions to highlight changes
+function compareTranscription(oldTranscription, newTranscription, index) {
+  var dmp = new diff_match_patch();
+  var text1 = oldTranscription;
+  var text2 = newTranscription;
+  
+  // Compare transcriptions
+  var d = dmp.diff_main(text1, text2);
+  
+  // Highlight changes
+  dmp.diff_cleanupSemantic(d);
+  var ds = dmp.diff_prettyHtml(d);
+  jQuery("#transcription-comparison-output-" + index).html(ds);
+}
+
+// Switches between image view and full view on the item page
+function switchItemPageView() {
+  if (jQuery('#full-view-container').css('display') == 'block') {
+    //switch to image view
+    jQuery('#full-view-container').css('display', 'none')
+    jQuery('#image-view-container').css('display', 'flex')
+    jQuery('.full-container').css('position', 'static')
+    jQuery('#item-view-switcher').css('position', 'absolute')
+    jQuery('#item-view-switcher').css('z-index', '9999991')
+    jQuery('#item-view-switcher').css('left', '50%')
+    jQuery('#item-view-switcher').css('top', '0')
+    jQuery('._tct_footer').css('display', 'none')
+
+    // move editor content
+    jQuery('#editor-tab').html(jQuery('#full-view-editor').html())
+    jQuery('#full-view-editor').html('')
+
+    // move tagging content
+    jQuery('#tagging-tab').html(jQuery('#full-view-tagging').html())
+    jQuery('#full-view-tagging').html('')
+
+    // move info content
+    jQuery('#info-tab').html(jQuery('#full-view-info').html())
+    jQuery('#full-view-info').html('')
+
+    // move help content
+    jQuery('#help-tab').html(jQuery('#full-view-help').html())
+    jQuery('#full-view-help').html('')
+
+    // move autoEnrichment content
+    jQuery('#autoEnrichment-tab').html(jQuery('#full-view-autoEnrichment').html())
+    jQuery('#full-view-autoEnrichment').html('')
+
+    // move help content
+    jQuery('#editor-help').html(jQuery('#full-view-help').html())
+    jQuery('#full-view-help').html('')
+  } else {
+    //switch to full view
+    jQuery('#full-view-container').css('display', 'block')
+    jQuery('#image-view-container').css('display', 'none')
+    jQuery('.full-container').css('position', 'relative')
+    jQuery('._tct_footer').css('display', 'block')
     
+    // move editor content
+    jQuery('#full-view-editor').html(jQuery('#editor-tab').html())
+    jQuery('#editor-tab').html('')
+    
+    // move tagging content
+    jQuery('#full-view-tagging').html(jQuery('#tagging-tab').html())
+    jQuery('#tagging-tab').html('')
+    
+    // move info content
+    jQuery('#full-view-info').html(jQuery('#info-tab').html())
+    jQuery('#info-tab').html('')
+    
+    // move help content
+    jQuery('#full-view-help').html(jQuery('#help-tab').html())
+    jQuery('#help-tab').html('')
+    
+    // move autoEnrichment content
+    jQuery('#full-view-autoEnrichment').html(jQuery('#autoEnrichment-tab').html())
+    jQuery('#autoEnrichment-tab').html('')
+  }
 }
