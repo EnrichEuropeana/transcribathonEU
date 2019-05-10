70b0f68703926822fff55a8140a1f63087f4396f
Marius Meller <marius_meller@hotmail.de>
2019-05-10 18:16:33 +0200

    Script to receive ajax posts to API

---
+++ wp-content/themes/transcribathon/admin/inc/custom_scripts/send_ajax_api_request.php
@@ -0,0 +1,37 @@
+<?php 
+include(str_repeat("../",(sizeof(explode("/",substr((string)getcwd(),strrpos((string)getcwd(),"/wp-content"),strlen((string)getcwd()))))-2))."../wp-load.php");
+require_once( $_SERVER["DOCUMENT_ROOT"].'/wp-admin/includes/post.php' );
+
+
+if(isset($_POST['type']) && isset($_POST['url']) &&isset($_POST['data'])){
+    // Set Post content
+    $data = array(
+    );
+    foreach ($_POST['data'] as $key => $value) {
+        $data[$key] = $value;
+    }
+    $postContent = json_encode($data);
+    
+    // Prepare new cURL resource
+    $ch = curl_init($_POST['url']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_POST['type']);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postContent);
+    
+    // Set HTTP Header for POST request 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($postContent))
+    );
+    
+    // Submit the POST request
+    $result = curl_exec($ch);
+
+    // Get response code
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    
+    // Close cURL session handle and return response code
+    curl_close($ch);
+    echo trim(json_encode($httpcode));
+}
